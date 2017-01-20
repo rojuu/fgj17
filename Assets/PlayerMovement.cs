1fbe1faac9fc60b1af62e55623f1bb59ef3e1ab8
Mika_lintu <nd.yuki.247@gmail.com>
2017-01-20 23:41:38 +0200

    Pelaajan liikkumista muokattu

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
     public Vector2 jump = new Vector2(0, 300);
     Rigidbody2D rigi;
+    public float holdTime = 0;
+    public float maxHoldTime = 0.2f;
 	
     void Start()
     {
@@ -12,13 +14,33 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown
-            ("space"))
+        /* if (Input.GetKeyDown("space"))
+         {
+             rigi.velocity = Vector2.zero;
+             rigi.AddForce(jump);
+         }*/
+        // Testing
+        if (Input.GetKey(KeyCode.Space))
         {
-            rigi.velocity = Vector2.zero;
-            rigi.AddForce(jump);
+            holdTime += Time.deltaTime;
+        }
+        else
+        {
+            if (holdTime >= maxHoldTime)
+                {
+                    rigi.velocity = Vector2.zero;
+                    rigi.AddForce(jump);
+                }
+                else if(holdTime > 0)
+                {
+                    rigi.velocity = Vector2.zero;
+                    rigi.AddForce(jump * holdTime * 5);
+                }
+            holdTime = 0;
+
         }
 	}
+
     void OnCollisionEnter2D(Collision2D other)
     {
         Destroy(gameObject);
