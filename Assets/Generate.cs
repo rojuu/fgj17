217d1e79a909c4014d0caf77a80d250287002f03
Mika_lintu <nd.yuki.247@gmail.com>
2017-01-20 21:54:30 +0200

    Pelaajan liike
    Esteet ja niiden liikkuminen

---
+++ Assets/Generate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+    public GameObject capsule;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("CreateObstacle", 1f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void CreateObstacle ()
+    {
+        Instantiate(capsule);
+       
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public Vector2 jump = new Vector2(0, 300);
+    Rigidbody2D rigi;
+	
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody2D>();
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown
+            ("space"))
+        {
+            rigi.velocity = Vector2.zero;
+            rigi.AddForce(jump);
+        }
+	}
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+    
+}
