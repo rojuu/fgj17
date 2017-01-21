7c643a4b851217e8d4297aa55f347af4c19c648b
Roni <Roni-OttoJuppi@kamk.fi>
2017-01-21 16:19:56 +0200

    joo

---
+++ Assets/ScoreUpdater.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreUpdater : MonoBehaviour {
+
+    Text text;
+    float score;
+
+	void Start ()
+    {
+        text = GetComponent<Text>();
+        if(text == null) { Destroy(this); }
+        score = 0;
+        text.text = "Score: " + score;
+	}
+
+    void Update()
+    {
+        if(score != GameManager.Instance.score)
+        {
+            score = GameManager.Instance.score;
+            text.text = "Score: " + score;
+        }
+    }
+}
