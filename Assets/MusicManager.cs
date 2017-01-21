c3bf68dffd5060b89b527034d85c4894247039c1
Roni <Roni-OttoJuppi@kamk.fi>
2017-01-21 22:51:11 +0200

    music

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MusicManager : MonoBehaviour
 {
     AudioClip GameMusic;
     AudioClip MenuMusic;
-    
+
+    AudioSource audioSource;
+
+    int lastSceneIndex;
+
     void Awake()
     {
         DontDestroyOnLoad(this);
@@ -14,6 +19,24 @@ public class MusicManager : MonoBehaviour
         GameMusic = Resources.Load<AudioClip>(GlobalVars.Instance.MusicGameName);
         MenuMusic = Resources.Load<AudioClip>(GlobalVars.Instance.MusicMenuName);
 
+        audioSource = GetComponent<AudioSource>();
+    }
 
+    void Update()
+    {
+        if (!audioSource.isPlaying || SceneManager.GetActiveScene().buildIndex != lastSceneIndex)
+        {
+            lastSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            audioSource.Stop();
+            if (SceneManager.GetActiveScene().name == GlobalVars.Instance.GameSceneName)
+            {
+                audioSource.clip = GameMusic;
+            }
+            else
+            {
+                audioSource.clip = MenuMusic;
+            }
+            audioSource.Play();
+        }
     }
 }
