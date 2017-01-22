c7591e479d1e4d522c6d05865f7fef221dedb0fa
Roni <Roni-OttoJuppi@kamk.fi>
2017-01-22 14:14:48 +0200

    everyting

---
+++ Assets/SpaceTextSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceTextSwitcher : MonoBehaviour {
+
+    public Sprite touchScreenText;
+    public Sprite spacebarText;
+
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+
+#if UNITY_ANDROID
+        sr.sprite = touchScreenText;
+
+#elif UNITY_IOS
+         sr.sprite = touchScreenText;
+
+#else
+        sr.sprite = spacebarText;
+
+#endif
+
+    }
+}
